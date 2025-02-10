1b96ccb8d5ffa2759124617d392c8aead476cc98
Danilo Just <92367836+jus-dan@users.noreply.github.com>
2025-02-10 11:04:36 +0100

    Melodie hinzugefügt

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 input.onButtonPressed(Button.A, function () {
+    music.setVolume(255)
+    music.play(music.stringPlayable("C5 C5 B E E B C B ", 400), music.PlaybackMode.UntilDone)
     lauflichtAn = 0
-    basic.showIcon(IconNames.Duck)
+    basic.showIcon(IconNames.Chessboard)
     strip.showRainbow(1, 360)
     strip.show()
 })
@@ -8,13 +10,15 @@ input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, 9))
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Happy)
     lauflichtAn = 1
 })
 let lauflichtAn = 0
 let strip: neopixel.Strip = null
+music.setVolume(255)
+music.play(music.stringPlayable("C5 C5 B E E B C B ", 400), music.PlaybackMode.UntilDone)
 strip = neopixel.create(DigitalPin.P14, 450, NeoPixelMode.RGB)
-basic.showIcon(IconNames.Skull)
+basic.showIcon(IconNames.Ghost)
 lauflichtAn = 0
 basic.forever(function () {
     if (lauflichtAn == 1) {
