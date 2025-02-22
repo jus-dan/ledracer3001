84ccdc53529f22bafdc576b97a10fcdc73ab3c02
Danilo Just <92367836+jus-dan@users.noreply.github.com>
2025-02-22 11:33:54 +0100

    update

--- main.ts
+++ main.ts
@@ -1,28 +1,56 @@
-input.onButtonPressed(Button.A, function () {
-    music.setVolume(255)
-    music.play(music.stringPlayable("C5 C5 B E E B C B ", 400), music.PlaybackMode.UntilDone)
-    lauflichtAn = 0
-    basic.showIcon(IconNames.Chessboard)
-    strip.showRainbow(1, 360)
-    strip.show()
+input.onPinPressed(TouchPin.P0, function () {
+    setup()
+    music.play(music.stringPlayable("G - G - G - C5 - ", 100), music.PlaybackMode.InBackground)
 })
-input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 9))
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    setup()
 })
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Happy)
-    lauflichtAn = 1
+input.onPinPressed(TouchPin.P2, function () {
+    strip_20.rotate(5)
+    strip_20.show()
 })
-let lauflichtAn = 0
-let strip: neopixel.Strip = null
-music.setVolume(255)
-music.play(music.stringPlayable("C5 C5 B E E B C B ", 400), music.PlaybackMode.UntilDone)
-strip = neopixel.create(DigitalPin.P14, 450, NeoPixelMode.RGB)
-basic.showIcon(IconNames.Ghost)
-lauflichtAn = 0
+input.onPinPressed(TouchPin.P1, function () {
+    strip_10.rotate(5)
+    strip_10.show()
+})
+function setup () {
+    strip_10 = neopixel.create(DigitalPin.P14, 450, NeoPixelMode.RGB)
+    strip_20 = neopixel.create(DigitalPin.P14, 450, NeoPixelMode.RGB)
+    strip_20.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip_20.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip_20.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip_20.setPixelColor(3, neopixel.colors(NeoPixelColors.Indigo))
+    strip_20.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
+    strip_20.setPixelColor(5, neopixel.colors(NeoPixelColors.Red))
+    strip_20.show()
+    strip_10.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    strip_10.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+    strip_10.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+    strip_10.setPixelColor(3, neopixel.colors(NeoPixelColors.Yellow))
+    strip_10.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+    strip_10.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+    strip_10.show()
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.showIcon(IconNames.Asleep)
+}
+let strip_10: neopixel.Strip = null
+let strip_20: neopixel.Strip = null
+setup()
 basic.forever(function () {
-    if (lauflichtAn == 1) {
-        strip.rotate(1)
-        strip.show()
-    }
+    strip_10.show()
+    basic.pause(1)
+    strip_20.show()
+    basic.pause(1)
+})
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(1000)
+})
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.pause(1000)
 })
